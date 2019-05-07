511ea11500fc61ba1be4b930548d440d619c91cb
Mariela <marims1011@gmail.com>
2019-05-07 17:30:04 -0600

    Algunos cambios y ajustes en la vista del chat

--- app/src/main/java/com/example/whatssap11/MainActivity.java
+++ app/src/main/java/com/example/whatssap11/MainActivity.java
@@ -175,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
                     while (! uTask.isSuccessful());
                     Uri u = uTask.getResult();
                     fotoPerfilCadena = u.toString();
-                    MensajeEnviar m = new MensajeEnviar(nombre.getText()+"ha actualizado su foto de perfil",u.toString(),nombre.getText().toString(),fotoPerfilCadena,"2",ServerValue.TIMESTAMP);
+                    MensajeEnviar m = new MensajeEnviar(nombre.getText()+ " ha actualizado su foto de perfil",u.toString(),nombre.getText().toString(),fotoPerfilCadena,"2",ServerValue.TIMESTAMP);
                     databaseReference.push().setValue(m);
                     Glide.with(MainActivity.this).load(u.toString()).into(fotoPerfil);
                 }
